14e0abdebee37f010e3d0a9f04d2325dd5d6052d
Uskova Elena <63116963+UskovaElena@users.noreply.github.com>
2021-04-09 13:42:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@
 int main() {
     int n1 = 0;
     int n2 = 0;
-    printf("Enter first number");
+    printf("Enter first number of N1");
     scanf("%d", &n1);
-    printf("Enter second number");
+    printf("Enter second number of N1");
     scanf("%d", &n2);
     Fraction a(n1, n2);  // 1/2
     Fraction b(a);
@@ -16,10 +16,10 @@ int main() {
     c = a + b;
     printf("+: %s", c.getValue());
     Fraction c2;
-    c2 = n1 / n2;
+    c2 = c / a;
     printf("/: %s", c2.getValue());
     Fraction c3;
-    c3 = n1 - n2;
+    c3 = c2 - c;
     printf("-: %s", c3.getValue());
     return 0;
 }
