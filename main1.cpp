192f35dc5f2c6cc791e09541f132ba27ae1bfe54
Uskova Elena <63116963+UskovaElena@users.noreply.github.com>
2021-04-09 13:45:26 +0300

    Add files via upload

---
+++ main1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "task1.h"
+#define _CRT_SECURE_NO_WARNINGS
+using namespace std;
+
+
+int main()
+{
+	unsigned int n = 0;
+	cin >> n;
+	cout << getPrime(n);
+}
---
+++ task1.cpp
@@ -0,0 +1,22 @@
+#include "task1.h"
+using namespace std;
+
+unsigned int getPrime(unsigned int n)
+{
+	int counter = 0;              //cчитает номер простого числа
+	int flag = 0;
+	int i = 1, j = 0;
+	while (counter != n)
+	{
+		flag = 0;
+		i += 1;
+		for (j = 2; j < i; j++)
+		{
+			if (i % j == 0)
+				flag = 1;
+		}
+		if (flag == 0)
+			counter += 1;
+	}
+	return i;
+}
